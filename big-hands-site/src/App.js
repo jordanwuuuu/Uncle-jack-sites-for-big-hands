37c26031cf3c065754b466f5a661b785175d9777
jordanwuuuu <jordanwu1996@gmail.com>
2023-11-04 13:56:40 +0000

    feat(04/11/23): add assets and basic placeholders

--- big-hands-site/src/App.js
+++ big-hands-site/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import lawyerHands from "./assets/Its-Always-Sunny-its-always-sunny-in-philadelphia-16087009-1600-900.jpeg";
+import newHands from "./assets/newHands.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jack Kelly's site for very professional Lawyers</h1>
+        <img src={newHands} className="professionalNewHands" alt="newHands" />
+        <p> Two very fine lawyers successful case</p>
       </header>
     </div>
   );
